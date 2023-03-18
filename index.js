aab914d7b86eef6b2ffff64a413f49b43a4f229a
Watheq Attrah <novaatrah@gmail.com>
2023-03-17 18:47:53 -0700

    Conditiones Complete

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  if (distance<=400){
+    return 'This one is on me!';
+    }
+    else if (distance > 400 && distance < 2000) {
+      return 'That will be twenty bucks.';
+    }
+    else if (distance >= 2000 && distance <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+    }
+    else {
+      return 'No can do.';
+    }
+  }
+scuberGreetingForFeet(2501);
 
-function switchOnCharmFromTip(){
+
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === 'NYC' ? "Ok, sounds good." : "No go.";
 }
+ternaryCheckCity(city);
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much."
+      break;
+    case 'not as generous':
+      return "Thank you."
+      break;
+    case 'thanks for everything':
+      return "Bye."
+      break;
+  }
+}
+switchOnCharmFromTip('generous');
